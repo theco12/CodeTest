b6761c9ba6a3d07df382cd87d65f214ddeba59fc
theco12 <107103821+theco12@users.noreply.github.com>
2023-05-12 10:25:03 +0900

    channelTalk

---
+++ channetalk.js
@@ -0,0 +1,9 @@
+ChannelIO("boot", {
+  pluginKey: "YOUR_PLUGIN_KEY", // fill your plugin key
+});
+
+ChannelService.loadScript();
+
+ChannelService.boot({
+  pluginKey: "YOUR_PLUGIN_KEY", // fill your plugin key
+});
